f94a610f654904d1bdc6394463307828a3b43fa1
Dorcas <dorcas25sg@gmail.com>
2017-04-17 17:13:32 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,32 @@
+library(shiny)
+library(leaflet)
+
+function(input, output) {
+  
+  data <- eventReactive(input$go, {
+    # do a search based on input
+    # within bounding box of philly and prioritizing by closest distance to our classroom first (see boundary.rect and focus.point)
+    # replaces spaces in input with %20 - just a convention for it to work
+    # readLines then reads the geojson file
+    searching2 <- readLines(gsub(" ","%20",paste0("http://search.mapzen.com/v1/search?api_key=mapzen-RbHXgXe&text=", input$popup,"&boundary.rect.min_lat=39.8670043945&boundary.rect.min_lon=-75.2802810669&boundary.rect.max_lat=40.1379623413&boundary.rect.max_lon=-74.9558258057&focus.point.lat=39.949487.215281&focus.point.lon=-75.199285")))
+  })
+
+  output$map <- renderLeaflet({
+    leaflet() %>%
+    addTiles() %>%
+      setView(lat = 39.9526, lng = -75.1652, zoom = 10) %>% 
+      addGeoJSON(data())
+  })
+      
+  output$fish <- renderText(input$popup)
+  
+  output$download <- downloadHandler(
+    filename = function() {
+      paste0("search-", input$popup, Sys.Date(), ".txt")
+    },
+    content = function(temp) {
+      writeLines(c(data()), temp)
+    }
+  )
+}
+
---
+++ ui.R
@@ -0,0 +1,86 @@
+library(shiny)
+library(leaflet)
+
+navbarPage("Philadelphia Locator", theme = "bootstrap.min.css",
+  tabPanel("Map",
+      sidebarPanel(
+        tags$img(height=120, width=120, style="display: block; margin-left: auto; margin-right: auto;", src="locating.png"),
+        h4(style = "text-align: center","Try the App!"),
+        p(" "),
+        textInput("popup", "Search for a place here:"),
+        p("e.g. 'Houston Hall' "),
+      actionButton(inputId = "go", label = "Search Now!"),
+      p(" ", tags$br()),
+      p("You can even download the search result GeoJson here:"),
+      downloadButton("download", "Download Now")
+      ),
+      mainPanel(
+        leafletOutput("map"),
+        h4("Searching For:"),
+        textOutput("fish")
+      )
+    ),
+  tabPanel("About",
+           sidebarLayout(
+             sidebarPanel(
+               h2("About the App")
+             ),
+             mainPanel(
+               h3("How does the Philadelphia Locator Work?"),
+               p("Using the Mapzen Search API, your search is connected to open-source tools and data, geocoded, to give you relevant places in the Philadelphia region"),
+               p("The search bounding box is set to"),
+               tags$ol(
+                 tags$li("Min Lat: 39.8670043945"), 
+                 tags$li("Min Long:-75.2802810669"), 
+                 tags$li("Max Lat: 40.1379623413"),
+                 tags$li("Max Long: -74.9558258057")
+               ),
+              p("Search reslts are then prioritized based on closest distance to Stephen A. Levin Building.")
+             )
+           )
+  ),
+  tabPanel("Resources",
+           sidebarLayout(
+             sidebarPanel(
+               h2("Resources"),
+               tags$ol(
+                 tags$li("Shiny"), 
+                 tags$li("Bootswatch"),
+                 tags$li("Leaflet"),
+                 tags$li("Mapzen Search"), 
+                 tags$li("Webalys Freebies")
+               )
+             ),
+             mainPanel(
+               h3("Shiny"),
+               p("Used as the web application framework for R to build this site!"),
+               tags$a(href="http://shiny.rstudio.com/", "Read more on Shiny"),
+               h3("Bootswatch"),
+               p("Used one of its free CSS!"),
+               tags$a(href="http://bootswatch.com/", "Read more on Bootswatch"),
+               h3("Leaflet"),
+               p("For maps and markers!"),
+               tags$a(href="https://rstudio.github.io/leaflet/", "Read more on Leaflet for R"),
+               h3("Mapzen Search"),
+               p("An API for the search function!"),
+               tags$a(href="http://mapzen.com/documentation/search/search/", "Read more on Mapzen"),
+               h3("Webalys Freebies"),
+               p("For the logo!"),
+               p("Icons made by", a(href="http://www.flaticon.com/authors/webalys-freebies", "Webalys Freebies"), "from", a(href="http://www.flaticon.com", "www.flaticon.com"), "is licensed by", a(href="http://creativecommons.org/licenses/by/3.0/", "CC 3.0 BY"))
+             )
+           )
+           
+  ),
+  tabPanel("Source Code",
+           sidebarLayout(
+             sidebarPanel(
+               h3("Source Code")
+             ),
+             mainPanel(
+               h4("Github"),
+               p("Source code used for the app may be found on", a(href="https://dorcas25sg.github.io/shiny/", "this repository"))
+             )
+           )
+           
+  )
+)
